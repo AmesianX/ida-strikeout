36dac8d5c5c0dd31f4a5734b88b83162607391b9
Elias Bachaalany <elias.bachaalany@gmail.com>
2025-10-08 06:50:49 -0700

    minor mods

--- prep-cmake.bat
+++ prep-cmake.bat
@@ -2,24 +2,9 @@
 
 setlocal
 
-:: EA64=YES
-if not exist build64 mkdir build64
-pushd build64
+if not exist build cmake -B build -A x64
 
-cmake -A x64 .. -DEA64=YES
-
-if "%2"=="" (set cfg=Release) else (set cfg=%2)
-if "%1"=="build64" cmake --build . --config %cfg%
-popd
-
-
-:: EA64=NO
-if not exist build mkdir build
-pushd build
-cmake -A x64 .. -DEA64=NO
-if "%2"=="" (set cfg=Release) else (set cfg=%2)
-if "%1"=="build" cmake --build . --config %cfg%
-popd
+if "%1"=="build" cmake --build build --config RelWithDebInfo
 
 echo.
 echo All done!
